b0c5d3005c4d8595be59d302691c4d70c9e29cd9
apkwizz <103306283+apkwizz@users.noreply.github.com>
2022-04-10 15:06:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,74 +33,33 @@
     <!-- Chart.js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 
-    <title>Covid-19 Tracker</title>
+    <title>The Way To Appreciate Media Player And Subtitles To Your Mobile Phones</title>
 </head>
 <body>
     <div class="container-fluid bg-light p-5 text-center my-3">
-        <h1 class="">Covid-19 Tracker</h1>
-        <h5 class="text-muted">An opensource project to keep track of all the COVID-19 cases around the world.</h5>
-    </div>
-
-    <div class="container my-5">
-        <div class="row text-center">
-            <div class="col-4 text-warning">
-                <h5>Confirmed</h5>
-                <?php echo $total_confirmed;?>
-            </div>
-            <div class="col-4 text-success">
-                <h5>Recovered</h5>
-                <?php echo $total_recovered;?>
-            </div>
-            <div class="col-4 text-danger">
-                <h5>Deceased</h5>
-                <?php echo $total_deaths;?>
-            </div>
-        </div>
+        <h1 class="">The Way To Appreciate Media Player And Subtitles To Your Mobile Phones</h1>
     </div>
 
     <div class="container bg-light p-3 my-5 text-center">
-        <h5 class="text-info">"Prevention is the Cure."</h5>
-        <p class="text-muted">Stay Indoors Stay Safe.</p>
-    </div>
+        <p class="text-muted">There are a whole lot of people who are wondering why they should download an iPhone program to perform their iPhone mobile games instead of simply using the default option Wi-Fi or 3G mobile web browser. The reasons are extremely obvious. It is more convenient to use. The user interface is quite a bit more engaging than navigating a default website <b><a href="https://apkwizz.com/">apkwizz</a></b>. What`s more, there is absolutely no need to download and install a program to utilize iPhone internet games instead of simply using the default Wi-Fi.</p>
+<p class="text-muted">Install the mobile program; after installation, a shortcut will then be created on the homescreen on your program, then you can open it and surf your favorite videos, today you do not need to download and then install the older version of MX Player from package installation procedure because you are still able to update it whenever you`re facing a technical issue with Google Play. But an extremely substantial problem is that most smartphones do not encourage the media player installed on them due to the fact that these old versions of the software aren`t optimized for the Android platform. Hence, most smartphone users have been left with the only option of using the default browsers. These browsers incorporate the WebKit-Based browsers like Safari, Android browser, and Symbian browsers. WebKit based browsers are extremely efficient but they are limited concerning the features they support.</p>
 
-    <canvas id="myChart"></canvas>
+<p class="text-muted">To access your latest version of the greatest media player program, you can just go to your device`s home screen. At the peak of the screen, you would see a gear wheel with three icons. The left one is for the settings menu, while the right one would point you to the file manager, from where you are able to pick the downloaded files. You would also notice a folder called"Downloads" which is where your latest model of the player ought to be located.</p>
 
-    <div class="container-fluid">
-        <div class="table-responsive">
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Countries</th>
-                        <th scope="col">Confirmed</th>
-                        <th scope="col">Recovered</th>
-                        <th scope="col">Deceased</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach($data as $key => $value){
-                            $increase = $value[$days_count]['confirmed'] - $value[$days_count_prev]['confirmed'];
-                    ?>
-                        <tr>
-                            <th scope="row"><?php echo $key?></th>
-                            <td>
-                                <?php echo $value[$days_count]['confirmed'];?>
-                                <?php if($increase != 0){ ?>
-                                    <small class="text-danger pl-3"><i class="fas fa-arrow-up"></i><?php echo $increase;?></small>  
-                                <?php } ?>    
-                            </td>
-                            <td><?php echo $value[$days_count]['recovered'];?></td>
-                            <td><?php echo $value[$days_count]['deaths'];?></td>
-                        </tr>
-                    <?php }?>
-                </tbody>
-            </table>
-        </div>
+<p class="text-muted">Once you have chosen the downloaded document, you would simply click on it and a list of options will appear on the left pane of the screen. Select"APK Download" and a dialogue box with 3 things should be shown. Select"Lantern","Certificate", and"OAuth", then enter all essential details. Wait for a while until the APK is downloaded.</p>
+
+<p class="text-muted">If you would like to take it one step farther, you can alter your smartphone`s default settings so that the a player starts up by itself each time it is used. This feature is known as the a player auto-play feature and it`s activated by default. To get this setting, you need to first enable the auto-play feature in your device. This approach is typically done by tapping on"Settings" >"General", then scrolling to"Play Store".</p>
+
+<p class="text-muted">If you want to take this further and enhance up your media player`s performance even further, you can use the Google play program which supports both the offline and community streaming. Just ensure that your smartphone has access to Wifi. As soon as you`ve enabled this networking attribute in your smartphone <i><a href="https://apkwizz.com/category/application">Application</a></i>, you can connect to the internet via your mobile device and flow videos from YouTube, Netflix and other live streaming programs just enjoy how you would if you had a real computer or laptop computer nearby.</p>
+
+<p class="text-muted">There are different ways in which the player may be utilized with these programs. By way of example, if you want to play movies and watch media straight from your smartphone, you may download the app known as"Mobile Multimedia App" or"Movies On Phone." This is specifically designed for use with the numerous tablets manufactured by Samsung, HTC and LG. The application works as a media player which you link to your own smartphones through Bluetooth. You may then watch videos you have downloaded from the internet or tv shows on your phone.</p>
+
+<p class="text-muted">In the same way, if you need fast playback and speedy buffering of videos and TV shows, you ought to use the movie player program with mx player and make the most of the a variety of gesture support. There are basically four types of gesture support which include the swipe gesture, the tap gesture, the dual tap along with the very long haul and hold gesture. With the availability of over 20 gestures, you are able to personalize the user interface of your video player using the various gesture options out there. The only thing that you need to make sure your smartphones are compatible with the Samsung programs store so that you may download the right gesture support.</p>
     </div>
 
     <footer class="footer mt-auto py-3 bg-light">
         <div class="container text-center">
-            <span class="text-muted">Copyright &copy;2020, <a href="https://packetcode.in" target="_blank">Packetcode</a></span>
+            <span class="text-muted">Copyright &copy;2022</span>
         </div>
     </footer>
 
